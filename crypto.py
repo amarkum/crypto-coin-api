ef7487ebdf6d94f798bead31313593ca32865a57
Amar Kumar <amar.mailsbox@gmail.com>
2020-07-15 00:13:33 +0530

    Updated program to find list of crypto currency

--- crypto.py
+++ crypto.py
@@ -4,7 +4,6 @@ params = {'apikey': '610D5CD6-06DA-4055-81A3-6F02D1747C59'}
 
 # Coins
 URL = "https://rest.coinapi.io/v1/exchangerate/USD"
-assets_URL = "https://rest.coinapi.io/v1/assets"
 
 response = requests.get(url=URL, params=params)
 data = response.json()
@@ -20,15 +19,24 @@ for coin in coins:
 # Currency Definition
 print("\nCurrency Definition\n")
 
+assets_URL = "https://rest.coinapi.io/v1/assets"
+
 assets_response = requests.get(url=assets_URL, params=params)
 assets_data = assets_response.json()
 
 coin_id = ""
 coin_name = ""
+total_crypto_currency = 0
 for coin in assets_data:
+    is_crypto = coin['type_is_crypto']
+    if is_crypto == 1:
+        total_crypto_currency += 1
+
     coin_id = coin['asset_id']
     try:
         coin_name = coin['name']
     except:
         coin_name = "N/A"
     print(coin_id, coin_name)
+
+print("Total Crypto Currency", total_crypto_currency)
